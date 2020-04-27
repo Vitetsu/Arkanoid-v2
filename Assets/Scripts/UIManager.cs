4e25dbbb7f08eaccaa4c4c026b7c3e11778c4aed
Vitetsu <vitetsu@yandex.ru>
2020-04-27 12:22:55 +0400

    replace file UIManager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text Target;
+    public Text ScoreText;
+    public Text LivesText;
+
+    public int Score { get; set; }
+
+    private void Awake()
     {
-        
+        Brick.OnBrickDestruction += OnBrickDestruction;
+        BricksManager.OnLevelLoaded += OnLevelLoaded;
+        GameManager.OnLiveLost += OnLiveLost;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        OnLiveLost(GameManager.Instance.AvailibleLives);
+    }
+
+    private void OnLiveLost(int remainingLives)
+    {
+        LivesText.text = $"LIVES: {remainingLives}";
+    }
+
+    private void OnLevelLoaded()
+    {
+        UpdateRemainingBricksText();
+        UpdateScoreText(0);
+    }
+
+    private void UpdateScoreText(int increment)
+    {
+        this.Score += increment;
+        String scoreString = this.Score.ToString().PadLeft(5, '0');
+        ScoreText.text = $"SCORE:{Environment.NewLine}{scoreString}";
+    }
+
+    private void OnBrickDestruction(Brick obj)
+    {
+        UpdateRemainingBricksText();
+        UpdateScoreText(10);
+    }
+
+    private void UpdateRemainingBricksText()
+    {
+        Target.text = $"TARGET:{Environment.NewLine}{BricksManager.Instance.RemainingBricks.Count} / {BricksManager.Instance.InitialBricksCount}";
+    }
+
+    private void OnDisable()
+    {
+        Brick.OnBrickDestruction -= OnBrickDestruction;
+        BricksManager.OnLevelLoaded -= OnLevelLoaded;
     }
 }
